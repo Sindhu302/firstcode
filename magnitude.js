9ae39c962b25b1e017fbfcc1d92a8110307dbacf
Sindhu302 <arepallisindhu283@gmail.com>
2024-06-20 21:12:42 +0530

    Initial Push

---
+++ magnitude.js
@@ -0,0 +1,13 @@
+const fs = require("fs");
+const magnitudestr = fs.readFileSync("Magnitude.txt").toString();
+const magnitude = magnitudestr.split("\n");
+// console.log(magnitude);
+for (let i = 0; i < magnitude.length; i++) {
+  magnitude[i] = Number(magnitude[i]);
+}
+
+// console.log(magnitude);
+
+module.exports = magnitude;
+
+
